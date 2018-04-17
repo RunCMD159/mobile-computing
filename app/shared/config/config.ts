3a1d58fefc55ac66889b9386e32dedceb8c8b8de
FSchmuck <florian.schmuck@edu.campus02.at>
2018-04-17 17:35:47 +0200

    [Server] (Relocation) - moved server to separate repository, added real azure apiUrl

--- app/shared/config/config.ts
+++ app/shared/config/config.ts
@@ -1,5 +1,4 @@
 export class Config {
-    static apiUrl:string = 'http://10.0.2.2:3000/api';
-    static appKey:string ;
+    static apiUrl:string = 'https://mocbackend.azurewebsites.net/api';
     static authHeader:string ;
 }
